ca4a9cf876cb210e20e74e64de0d4f126a4fbf61
Daniel Higuero <daniel@daisho.group>
2019-05-24 12:44:38 +0200

    NP-1365 Inventory Events

---
+++ pkg/queue/inventory/events/manager.go
@@ -0,0 +1,145 @@
+package events
+
+import (
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	"github.com/nalej/derrors"
+	"github.com/nalej/grpc-bus-go"
+	"github.com/nalej/grpc-inventory-go"
+	"github.com/nalej/grpc-inventory-manager-go"
+	"github.com/nalej/nalej-bus/pkg/bus"
+	"github.com/nalej/nalej-bus/pkg/queue"
+	"github.com/rs/zerolog/log"
+	"golang.org/x/net/context"
+)
+
+const (
+	InventoryEventsTopic="nalej/inventory/events"
+)
+
+type InventoryEventsProducer struct {
+	producer bus.NalejProducer
+}
+
+// Create a new producer for the application operations topic
+// params:
+//  client to be used
+//  name of the producer
+// return:
+//  built producer
+func NewInventoryEventsProducer (client bus.NalejClient, name string) (*InventoryEventsProducer, derrors.Error) {
+	prod, err := client.BuildProducer(name, InventoryEventsTopic)
+	if err != nil {
+		return nil, err
+	}
+	return &InventoryEventsProducer{producer: prod}, nil
+}
+
+// Generic function that decides what to send depending on the object type.
+func (m InventoryEventsProducer) Send(ctx context.Context, msg proto.Message) derrors.Error {
+
+	var  wrapper grpc_bus_go.InventoryEvents
+
+	switch x := msg.(type) {
+	case *grpc_inventory_manager_go.AgentIds:
+		wrapper = grpc_bus_go.InventoryEvents{
+			Event: &grpc_bus_go.InventoryEvents_AgentIds{x},
+		}
+	case *grpc_inventory_go.EdgeControllerId:
+		wrapper = grpc_bus_go.InventoryEvents{
+			Event: &grpc_bus_go.InventoryEvents_EdgeControllerId{x}}
+	case *grpc_inventory_manager_go.EICStartInfo:
+		wrapper = grpc_bus_go.InventoryEvents{
+			Event: &grpc_bus_go.InventoryEvents_EicStart{x}}
+	default:
+		return derrors.NewInvalidArgumentError("invalid proto message type")
+	}
+
+	payload, err := queue.MarshallPbMsg(&wrapper)
+	if err != nil {
+		return err
+	}
+
+	err = m.producer.Send(ctx, payload)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// InventoryEventsConsumer to receive message from the InventoryEvents topic
+type InventoryEventsConsumer struct {
+	Consumer bus.NalejConsumer
+	Config ConfigInventoryEventsConsumer
+}
+
+// Struct designed to config a consumer defining what actions to perform depending on the incoming object.
+type ConfigInventoryEventsConsumer struct {
+	// ChAgentsIds with the channel to receive agent ids
+	ChAgentIds chan *grpc_inventory_manager_go.AgentIds
+	// ChEdgeControllerId with the channel to receive edge inventory ids
+	ChEdgeControllerId chan *grpc_inventory_go.EdgeControllerId
+	// ChEICStart with the channel to receive EIC start notifications.
+	ChEICStart chan *grpc_inventory_manager_go.EICStartInfo
+	// object types to be considered for consumption
+	ToConsume ConsumableStructsInventoryEventsConsumer
+}
+
+// Data struct indicating what data structures available in this topic will be accepted.
+type ConsumableStructsInventoryEventsConsumer struct {
+	// AgentIds consumption
+	AgentIds bool
+	// EdgeControllerId consumption
+	EdgeControllerId bool
+	// EICStartInfo consumption
+	EICStartInfo bool
+}
+
+func NewInventoryEventsConsumer (client bus.NalejClient, name string, exclusive bool, config ConfigInventoryEventsConsumer) (*InventoryEventsConsumer, derrors.Error) {
+	consumer, err := client.BuildConsumer(name, InventoryEventsTopic, exclusive)
+	if err != nil {
+		return nil, err
+	}
+
+	return &InventoryEventsConsumer{Consumer: consumer,Config: config}, nil
+}
+
+// Consume any of the possible objects that can be sent to this queue and send it to the corresponding channel.
+func (c InventoryEventsConsumer) Consume(ctx context.Context) derrors.Error{
+	msg, err := c.Consumer.Receive(ctx)
+	if err != nil {
+		return err
+	}
+
+	target := &grpc_bus_go.InventoryEvents{}
+
+	derr := queue.UnmarshallPbMsg(msg, target)
+	if derr != nil {
+		return derr
+	}
+
+	switch x := target.Event.(type) {
+	case *grpc_bus_go.InventoryEvents_AgentIds:
+		if  c.Config.ToConsume.AgentIds {
+			c.Config.ChAgentIds <- x.AgentIds
+		}
+	case *grpc_bus_go.InventoryEvents_EdgeControllerId:
+		if c.Config.ToConsume.EdgeControllerId {
+			c.Config.ChEdgeControllerId <- x.EdgeControllerId
+		}
+	case *grpc_bus_go.InventoryEvents_EicStart:
+		if c.Config.ToConsume.EICStartInfo {
+			c.Config.ChEICStart <- x.EicStart
+		}
+	case nil:
+		errMsg := "received nil entry"
+		log.Error().Msg(errMsg)
+		return derrors.NewInvalidArgumentError(errMsg)
+	default:
+		errMsg := fmt.Sprintf("unknown object type in %s",InventoryEventsTopic)
+		log.Error().Interface("type",x).Msg(errMsg)
+		return derrors.NewInvalidArgumentError(errMsg)
+	}
+	return nil
+}
