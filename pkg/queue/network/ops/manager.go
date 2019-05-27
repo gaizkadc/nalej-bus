8d108f3a75dabd16aa5ef60a61a4dd6dc6c55bf2
Juan <juan@daisho.group>
2019-05-27 17:39:00 +0200

    Added network ops manager.

---
+++ pkg/queue/network/ops/manager.go
@@ -0,0 +1,136 @@
+/*
+ * Copyright (C) 2019 Nalej - All Rights Reserved
+ */
+
+package ops
+
+import (
+    "context"
+    "fmt"
+    "github.com/golang/protobuf/proto"
+    "github.com/nalej/derrors"
+    "github.com/nalej/grpc-bus-go"
+    "github.com/nalej/grpc-network-go"
+    "github.com/nalej/nalej-bus/pkg/bus"
+    "github.com/nalej/nalej-bus/pkg/queue"
+    "github.com/rs/zerolog/log"
+)
+
+const (
+    NetworkOpsTopic="nalej/network/ops"
+)
+
+type NetworkOpsProducer struct {
+    producer bus.NalejProducer
+}
+
+// Create a new producer for the application operations topic
+// params:
+//  client to be used
+//  name of the producer
+// return:
+//  built producer
+func NewNetworkOpsProducer (client bus.NalejClient, name string) (*NetworkOpsProducer, derrors.Error) {
+    prod, err := client.BuildProducer(name, NetworkOpsTopic)
+    if err != nil {
+        return nil, err
+    }
+    return &NetworkOpsProducer{producer: prod}, nil
+}
+
+// Generic function that decides what to send depending on the object type.
+func (m NetworkOpsProducer) Send(ctx context.Context, msg proto.Message) derrors.Error {
+
+    var  wrapper grpc_bus_go.NetworkOps
+
+    switch x := msg.(type) {
+    case *grpc_network_go.AuthorizeMemberRequest:
+        wrapper = grpc_bus_go.NetworkOps{Operation: &grpc_bus_go.NetworkOps_AuthorizeMemberRequest{x}}
+    case *grpc_network_go.DisauthorizeMemberRequest:
+        wrapper = grpc_bus_go.NetworkOps{Operation: &grpc_bus_go.NetworkOps_DisauthorizeMemberRequest{x}}
+    default:
+        return derrors.NewInvalidArgumentError("invalid proto message type")
+    }
+
+    payload, err := queue.MarshallPbMsg(&wrapper)
+    if err != nil {
+        return err
+    }
+
+    err = m.producer.Send(ctx, payload)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+// Network consumer
+type NetworkOpsConsumer struct {
+    Consumer bus.NalejConsumer
+    Config ConfigNetworkOpsConsumer
+}
+
+// Struct designed to config a consumer defining what actions to perform depending on the incoming object.
+type ConfigNetworkOpsConsumer struct {
+    // channel to receive authorize requests
+    ChAuthorizeMembersRequest chan *grpc_network_go.AuthorizeMemberRequest
+    // channel to receive disauthorize requests
+    ChDisauthorizeMembersRequest chan *grpc_network_go.DisauthorizeMemberRequest
+    // object types to be considered for consumption
+    ToConsume ConsumableStructsNetworkOpsConsumer
+}
+
+// Data struct indicating what data structures available in this topic will be accepted.
+type ConsumableStructsNetworkOpsConsumer struct {
+    // Consume authorize members
+    AuthorizeMember bool
+    // Consume disauthorize members
+    DisauthorizeMember bool
+}
+
+
+func NewNetworkOpsConsumer (client bus.NalejClient, name string, exclusive bool, config ConfigNetworkOpsConsumer) (*NetworkOpsConsumer, derrors.Error) {
+    consumer, err := client.BuildConsumer(name, NetworkOpsTopic, exclusive)
+    if err != nil {
+        return nil, err
+    }
+
+    return &NetworkOpsConsumer{Consumer: consumer,Config: config}, nil
+}
+
+
+// Consume any of the possible objects that can be sent to this queue and send it to the corresponding channel.
+func (c NetworkOpsConsumer) Consume(ctx context.Context) derrors.Error{
+    msg, err := c.Consumer.Receive(ctx)
+    if err != nil {
+        return err
+    }
+
+    target := &grpc_bus_go.NetworkOps{}
+
+    derr := queue.UnmarshallPbMsg(msg, target)
+    if derr != nil {
+        return derr
+    }
+
+    switch x := target.Operation.(type) {
+    case *grpc_bus_go.NetworkOps_AuthorizeMemberRequest:
+        if  c.Config.ToConsume.AuthorizeMember {
+            c.Config.ChAuthorizeMembersRequest <- x.AuthorizeMemberRequest
+        }
+    case *grpc_bus_go.NetworkOps_DisauthorizeMemberRequest:
+        if  c.Config.ToConsume.DisauthorizeMember {
+            c.Config.ChDisauthorizeMembersRequest <- x.DisauthorizeMemberRequest
+        }
+    case nil:
+        errMsg := "received nil entry"
+        log.Error().Msg(errMsg)
+        return derrors.NewInvalidArgumentError(errMsg)
+    default:
+        errMsg := fmt.Sprintf("unknown object type in %s",NetworkOpsTopic)
+        log.Error().Interface("type",x).Msg(errMsg)
+        return derrors.NewInvalidArgumentError(errMsg)
+    }
+    return nil
+}
