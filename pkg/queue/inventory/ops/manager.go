27a3f00d27cc6b35aee0bba702a9f083e77408c7
Carmen de Lope <cdelope@daisho.group>
2019-06-07 13:49:27 +0200

    NP-1504 inventory ops added

---
+++ pkg/queue/inventory/ops/manager.go
@@ -0,0 +1,137 @@
+package ops
+
+import (
+	"context"
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	"github.com/nalej/derrors"
+	"github.com/nalej/grpc-bus-go"
+	"github.com/nalej/grpc-inventory-manager-go"
+	"github.com/nalej/nalej-bus/pkg/bus"
+	"github.com/nalej/nalej-bus/pkg/queue"
+	"github.com/rs/zerolog/log"
+)
+
+const (
+	InventoryOpsTopic="nalej/inventory/ops"
+)
+
+// Producer
+type InventoryOpsProducer struct {
+	producer bus.NalejProducer
+}
+
+// Create a new producer for the application operations topic
+// params:
+//  client to be used
+//  name of the producer
+// return:
+//  built producer
+func NewInventoryOpsProducer (client bus.NalejClient, name string) (*InventoryOpsProducer, derrors.Error) {
+	prod, err := client.BuildProducer(name, InventoryOpsTopic)
+	if err != nil {
+		return nil, err
+	}
+	return &InventoryOpsProducer{producer: prod}, nil
+}
+
+// Generic function that decides what to send depending on the object type.
+func (m InventoryOpsProducer) Send(ctx context.Context, msg proto.Message) derrors.Error {
+
+	var  wrapper grpc_bus_go.InventoryOps
+
+	switch x := msg.(type) {
+	case *grpc_inventory_manager_go.AgentOpResponse:
+		wrapper = grpc_bus_go.InventoryOps{ Operation: &grpc_bus_go.InventoryOps_AgentOpResponse{x}}
+	default:
+		return derrors.NewInvalidArgumentError("invalid proto message type")
+	}
+
+	payload, err := queue.MarshallPbMsg(&wrapper)
+	if err != nil {
+		return err
+	}
+
+	err = m.producer.Send(ctx, payload)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Consumer
+
+type InventoryOpsConsumer struct {
+	Consumer bus.NalejConsumer
+	Config ConfigInventoryOpsConsumer
+}
+
+func NewInventoryOpsConsumer (client bus.NalejClient, name string, exclusive bool, config ConfigInventoryOpsConsumer) (*InventoryOpsConsumer, derrors.Error) {
+	consumer, err := client.BuildConsumer(name, InventoryOpsTopic, exclusive)
+	if err != nil {
+		return nil, err
+	}
+
+	return &InventoryOpsConsumer{Consumer: consumer,Config: config}, nil
+}
+
+// Data struct indicating what data structures available in this topic will be accepted.
+type ConsumableStructsInventoryOpsConsumer struct {
+	// Consume agent operation response
+	AgentOpResponse bool
+}
+
+
+// Struct designed to config a consumer defining what actions to perform depending on the incoming object.
+type ConfigInventoryOpsConsumer struct {
+	// channel to receive operation Responses
+	ChAgentOpResponse chan *grpc_inventory_manager_go.AgentOpResponse
+	// object types to be considered for consumption
+	ToConsume ConsumableStructsInventoryOpsConsumer
+}
+
+// Create a new configuration structure for a given channel size
+// params:
+//  size of channels
+// return:
+//  instance of a configuration object
+func NewConfigInventoryOpsConsumer(size int, toConsume ConsumableStructsInventoryOpsConsumer) ConfigInventoryOpsConsumer {
+	chOpResponse := make(chan *grpc_inventory_manager_go.AgentOpResponse, size)
+
+	return ConfigInventoryOpsConsumer{
+		ChAgentOpResponse: chOpResponse,
+		ToConsume: toConsume,
+	}
+}
+
+// Consume any of the possible objects that can be sent to this queue and send it to the corresponding channel.
+func (c InventoryOpsConsumer) Consume(ctx context.Context) derrors.Error{
+	msg, err := c.Consumer.Receive(ctx)
+	if err != nil {
+		return err
+	}
+
+	target := &grpc_bus_go.InventoryOps{}
+
+	derr := queue.UnmarshallPbMsg(msg, target)
+	if derr != nil {
+		return derr
+	}
+
+	switch x := target.Operation.(type) {
+	case *grpc_bus_go.InventoryOps_AgentOpResponse:
+		if  c.Config.ToConsume.AgentOpResponse {
+			c.Config.ChAgentOpResponse <- x.AgentOpResponse
+		}
+	case nil:
+		errMsg := "received nil entry"
+		log.Error().Msg(errMsg)
+		return derrors.NewInvalidArgumentError(errMsg)
+	default:
+		errMsg := fmt.Sprintf("unknown object type in %s",InventoryOpsTopic)
+		log.Error().Interface("type",x).Msg(errMsg)
+		return derrors.NewInvalidArgumentError(errMsg)
+	}
+	return nil
+}
